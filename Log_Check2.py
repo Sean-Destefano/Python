133bd643975bd1c54eda19d528e8143599b9eecc
Sean De Stefano <seanadestefano@gmail.com>
2017-08-30 10:10:53 -0400

    Added comments

--- Log_Check2.py
+++ Log_Check2.py
@@ -1,10 +1,12 @@
 #Script to check logfiles for WINPE+OOBE matching SN's, and add good items to list
+#Uses my powershell logging script
 import codecs
 winpe = []
 oobe = []
 left = []
 #Attatch corresponding log items to correct list
-with codecs.open('C:\\Users\\Admin\\Desktop\\Project.txt', 'r', 'utf-16') as log:
+#INSERT LOGFLE IN FIRST QUOTES
+with codecs.open('LogfileHere', 'r', 'utf-16') as log:
 	lines = log.read().splitlines()
 for line in lines:
 	if "WINPE" in line:
@@ -16,11 +18,13 @@ for x in winpe:
 	if any(x[:7] in y for y in lines):
 		if any(x[:7] in y for y in oobe):
 			left.append(x[:24])
-output = open('c:\Test.txt', 'r+')
+#Insert output file below
+output = open('OUTPUTFILEHERE', 'r+')
 for item in left:
 	output.write(item+'\n')
 print(left)
 #Check for leftover items
+#Can easily add log export here as well
 for x in lines:
 	if not any(x[:7] in y for y in oobe):
 		print(x)
